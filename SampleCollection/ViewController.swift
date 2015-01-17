e06c3af7beebf07e8cf73c947f39e8a7be439c7e
fuji2013 <fuhi1983@gmail.com>
2015-01-17 12:22:56 +0900

    initial commitment

---
+++ SampleCollection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleCollection
+//
+//  Created by fuhi1983 on 2015/01/17.
+//  Copyright (c) 2015年 sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

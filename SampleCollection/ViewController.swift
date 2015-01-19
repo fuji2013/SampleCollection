6bbc12bdb5a3e0d457393a3b102299d447f6d554
fuji2013 <fuhi1983@gmail.com>
2015-01-20 02:34:37 +0900

    画面回転したらCollectionViewのスクロール方向を変更する

--- SampleCollection/ViewController.swift
+++ SampleCollection/ViewController.swift
@@ -10,23 +10,23 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource {
     var collectionView:UICollectionView!
+    let flowLayout = UICollectionViewFlowLayout()  // 3.画面回転したらCollectionViewのスクロール方向を変更する
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         // レイアウト作成
-        let flowLayout = UICollectionViewFlowLayout()
         flowLayout.scrollDirection = .Vertical
         flowLayout.minimumInteritemSpacing = 5.0
         flowLayout.minimumLineSpacing = 5.0
         flowLayout.itemSize = CGSizeMake(100, 100)
-        flowLayout.headerReferenceSize = CGSizeMake(0, 50)  // 2.ヘッダー追加のため
+        flowLayout.headerReferenceSize = CGSizeMake(0, 50)
         
         // コレクションビュー作成
         collectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: flowLayout)
         collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-        collectionView.registerClass(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "header")  // 2.ヘッダー追加のため
+        collectionView.registerClass(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "header")
         collectionView.dataSource = self
         view.addSubview(collectionView)
         
@@ -53,6 +53,44 @@ class ViewController: UIViewController, UICollectionViewDataSource {
         // Dispose of any resources that can be recreated.
     }
     
+    // 3.画面回転したらCollectionViewのスクロール方向を変更する
+    // 回転を有効にするか
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    // 3.画面回転したらCollectionViewのスクロール方向を変更する
+    // 回転できる方向
+    override func supportedInterfaceOrientations() -> Int {
+        return Int(UIInterfaceOrientationMask.All.rawValue) // 値がUIntなので、Int型に変換
+    }
+    
+    // 3.画面回転したらCollectionViewのスクロール方向を変更する
+    // 画面回転する前の処理
+    override func willAnimateRotationToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
+        
+        // 3.画面回転したらCollectionViewのスクロール方向を変更する
+        // 縦画面では縦にスクロール、横画面では横にスクロールする。
+        // 処理が行われたかわかりにくいので、collectionViewの背景色を画面方向によって変更
+        if toInterfaceOrientation == .Portrait
+            || toInterfaceOrientation == .PortraitUpsideDown{
+                
+                flowLayout.scrollDirection = .Vertical
+                collectionView.backgroundColor = UIColor.blackColor()
+                
+        }else if toInterfaceOrientation == .LandscapeLeft
+            || toInterfaceOrientation == .LandscapeRight{
+                
+                flowLayout.scrollDirection = .Horizontal
+                collectionView.backgroundColor = UIColor.whiteColor()
+                
+        }else{
+            // 画面回転方向がunknownのときは縦にスクロール
+            flowLayout.scrollDirection = .Vertical
+            collectionView.backgroundColor = UIColor.orangeColor()
+        }
+    }
+    
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 100
     }
@@ -64,7 +102,6 @@ class ViewController: UIViewController, UICollectionViewDataSource {
         return cell
     }
     
-    // 2.ヘッダー追加のため
     func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
         
         let headerReusableView = collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionHeader, withReuseIdentifier: "header", forIndexPath: indexPath) as UICollectionReusableView
